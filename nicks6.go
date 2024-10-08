86ccbfbcd2593655577dc506c897fee7092f7bab
AshKmo <ashkmo33@gmail.com>
2024-10-08 13:47:45 +1100

    work hard, hardy hard

--- nicks6.go
+++ nicks6.go
@@ -1,21 +1,13 @@
 package nicks6
 
 import (
-	"reflect"
 	"strings"
 	"strconv"
+	"fmt"
 )
 
-type Element struct {
-	Type reflect.Type
-	Value any
+type Unit interface {
 }
-
-type Context map[string]Element
-
-type Bracket byte
-
-type Special byte
 
 type Null struct{}
 
@@ -24,60 +16,87 @@ type Fraction struct {
 	Denominator uint
 }
 
-func gcd(a int, b int) int {
+type Operator string
+
+type Bracket byte
+
+type Special byte
+
+type Symbol string
+
+type String string
+
+type List []Unit
+
+type Dictionary map[string]Unit
+
+type Function func(Unit) Unit
+
+func gcd(a uint, b uint) uint {
 	if b == 0 {
 		return a
 	}
 	return gcd(b, a % b)
 }
 
-func simplify(n int, d int) (int, int) {
+func simplify(n uint, d uint) (uint, uint) {
 	g := gcd(n, d)
-	if g < 0 {
+	if (g < 0) {
 		g = -g
 	}
-	if g == 0 {
+	if (g == 0) {
 		g = 1
 	}
+
 	return n / g, d / g
 }
 
-type String string
-
-type List []Element
-
-type Dictionary map[string]Element
-
-type Closure struct {
-	Ctx Context
-	Variable string
-	Expression Element
+func abs(x int) uint {
+	if x < 0 {
+		return uint(-x)
+	}
+	return uint(x)
 }
 
-func Lex(script string) []Element {
-	var tokens []Element
+func sign(x int) int {
+	if x < 0 {
+		return -1
+	}
+	return 1
+}
 
+func mul(a Fraction, b Fraction) Fraction {
+	sign := sign(a.Numerator) * sign(b.Numerator)
+	an, ad := simplify(abs(a.Numerator), b.Denominator)
+	bn, bd := simplify(abs(b.Numerator), a.Denominator)
+
+	return Fraction{int(an) * int(bn) * sign, ad * bd}
+}
+
+func Lex(script string) []Unit {
+	tokens := []Unit{}
 	var token strings.Builder
-	var ctype reflect.Type
-
-	i := 0
+	
+	isOperator := false
 
 	endToken := func() {
-		ctype = reflect.TypeOf(Null{})
-
 		if token.Len() > 0 {
-			tokens = append(tokens, Element{ctype, token.String()})
+			if isOperator {
+				tokens = append(tokens, Operator(token.String()))
+			} else {
+				tokens = append(tokens, Symbol(token.String()))
+			}
 		}
 
 		token.Reset()
 	}
 
-	for ; i < len(script); i++ {
+	// go through each character and handle it
+	for i := 0; i < len(script); i++ {
 		c := script[i]
 
+		// comment skipper
 		if c == '#' {
-			i++
-
 			escaped := false
 
 			for ; i < len(script); i++ {
@@ -98,14 +117,13 @@ func Lex(script string) []Element {
 			continue
 		}
 
+		// string maker
 		if c == '"' {
 			endToken()
 
-			i++
-
-			var str strings.Builder
-
 			escaped := false
+
+			i++
 
 			for ; i < len(script); i++ {
 				c := script[i]
@@ -121,93 +139,164 @@ func Lex(script string) []Element {
 
 				if escaped {
 					switch c {
-						case 'n':
-							str.WriteRune('\n')
-						case 't':
-							str.WriteRune('\t')
-						default:
-							str.WriteByte(c)
+					case 't':
+						token.WriteByte('\t')
+					case 'n':
+						token.WriteByte('\n')
+					default:
+						token.WriteByte(c)
 					}
 				} else {
-					str.WriteByte(c)
+					token.WriteByte(c)
 				}
 
 				escaped = false
 			}
 
-			tokens = append(tokens, Element{reflect.TypeOf(String("")), String(str.String())})
+			tokens = append(tokens, String(token.String()))
+
+			token.Reset()
 
 			continue
 		}
 
 		switch c {
-			case ' ', '\t', '\n':
-				endToken()
+		case '\n', '\t', ' ':
+			endToken()
 
-			case '(', ')', '[', ']', '{', '}':
-				endToken()
-				tokens = append(tokens, Element{reflect.TypeOf(Bracket('(')), c})
+		case '(', ')', '[', ']', '{', '}':
+			endToken()
+			tokens = append(tokens, Bracket(c))
 
-			case '\\', ':', ',':
-				endToken()
-				tokens = append(tokens, Element{reflect.TypeOf(Special(',')), c})
+		case ':', ',', '\\':
+			endToken()
+			tokens = append(tokens, Special(c))
 
-			case '_':
-				endToken()
-				tokens = append(tokens, Element{reflect.TypeOf(Null{}), Null{}})
+		case '_':
+			endToken()
+			tokens = append(tokens, Null{})
 
-			case '@':
-				endToken()
-				tokens = append(tokens, Element{reflect.TypeOf(Fraction{}), Fraction{1, 0}})
+		case '@':
+			endToken()
+			tokens = append(tokens, Fraction{1, 0})
 
-			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-				endToken()
-				
-				var ns strings.Builder
-				dec := -1
-				digits := 0
+		// number parser
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			endToken()
 
-				for ; i < len(script); i++ {
-					c := script[i]
+			digits := 0
+			dec := 0
 
-					if c == '.' && dec < 0 {
-						dec = digits
-						continue
-					}
+			L:
+			for ; i < len(script); i++ {
+				c := script[i]
 
-					if 48 <= c && c <= 57 {
-						ns.WriteByte(c)
-						digits++
-						continue
-					}
-
-					if c == '_' {
-						continue
-					}
-
-					break
+				switch c {
+				case '.':
+					dec = digits
+				case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+					digits++
+					token.WriteByte(c)
+				case '_':
+				default:
+					break L
 				}
+			}
 
-				num, err := strconv.Atoi(ns.String())
-				if err != nil {
-					panic(err)
+			numString := token.String()
+
+			token.Reset()
+
+			var num uint = 0
+			var m uint = 1
+			for i := len(numString) - 1; i >= 0; i-- {
+				num += m * (uint(numString[i]) - 48)
+				m *= 10
+			}
+
+			var denom uint = 1
+			if dec != 0 {
+				for i := 0; i < digits - dec; i++ {
+					denom *= 10
 				}
+			}
 
-				var denom int
-				if dec < 0 {
-					denom = 1
-				} else {
-					denom = 1
-					for x := 0; x < dec - digits; x++ {
-						denom = denom * 10
-					}
-				}
+			num, denom = simplify(num, denom)
 
-				num, denom = simplify(num, denom)
+			tokens = append(tokens, Fraction{int(num), denom})
 
-				tokens = append(tokens, Element{reflect.TypeOf(Fraction{}), Fraction{num, uint(denom)}})
+		case '+', '-', '*', '/', '<', '>', '=', '.', '~', '&', '|', '^':
+			if !isOperator {
+				endToken()
+				isOperator = true
+			}
+
+			token.WriteByte(c)
+
+		default:
+			if isOperator {
+				endToken()
+				isOperator = false
+			}
+
+			token.WriteByte(c)
 		}
 	}
 
+	endToken()
+
 	return tokens
+}
+
+func Parse(tokens []Unit) Unit {
+	// TODO
+	return nil
+}
+
+func Evaluate(exp Unit) Unit {
+	// TODO
+	return nil
+}
+
+func Interpret(s string) Unit {
+	return Evaluate(Parse(Lex(s)))
+}
+
+func Pretty(u Unit) string {
+	switch v := u.(type) {
+	case Null:
+		return "_"
+	case Symbol:
+		return string(v)
+	case Operator:
+		return string(v)
+	case Bracket:
+		return string(v)
+	case String:
+		r := string(v)
+		olds := []string{"\\", "\"", "\n", "\r", "\t"}
+		news := []string{"\\\\", "\\\"", "\\n", "\\r", "\\t"}
+		for i := 0; i < len(olds); i++ {
+			r = strings.Replace(r, olds[i], news[i], -1)
+		}
+		return "\"" + r + "\""
+	case Fraction:
+		if v.Denominator == 1 {
+			return strconv.Itoa(v.Numerator)
+		}
+		return fmt.Sprintf("(%d / %d)", v.Numerator, v.Denominator)
+	case List:
+		r := "["
+		for i, e := range v {
+			r += Pretty(e)
+
+			if i + 1 < len(v) {
+				r += ", "
+			}
+		}
+
+		return r + "]"
+	}
+
+	return "<UNKNOWN>"
 }
--- nicks6_test.go
+++ nicks6_test.go
@@ -2,7 +2,18 @@ package nicks6
 
 import (
 	"testing"
+	"fmt"
 )
 
-func TestInterpret(t *testing.T) {
+func TestFull(t *testing.T) {
+	script := ""
+
+	tokens := Lex(script)
+	fmt.Println(Pretty(List(tokens)))
+
+	parsed := Parse(tokens)
+	fmt.Println(Pretty(parsed))
+
+	res := Evaluate(parsed)
+	fmt.Println(Pretty(res))
 }
